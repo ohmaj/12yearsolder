b443f9face7e33f19a116adef3a441927386c578
Joel Ohman <Joel Ohman>
2016-05-10 12:10:26 -0500

    initial

---
+++ 12yearsodler/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12yearsodler
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int age;
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.age = getAge(dateOfBirth);
+
+        }
+        public int getAge(DateTime dob)
+        {
+            int age = 0;
+            DateTime today = DateTime.Today;
+
+            age = today.Year - dob.Year;
+            if (dob.AddYears(age) > today)
+            {
+                age--;
+            }
+            return age;
+        }
+        public static Person operator ++(Person person )
+            {
+            person.age = person.age + 12;
+            return person;
+            }
+    }
+}
---
+++ 12yearsodler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12yearsodler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("jo","shmo",Convert.ToDateTime("11/10/1985"));
+            Console.WriteLine(person.FirstName+" "+person.LastName+" is "+person.age+" right now!");
+            person++;
+            Console.WriteLine(person.FirstName + " " + person.LastName + " will be " + person.age + " 12 years from now!");
+            Console.ReadKey();
+            
+        }
+    }
+}
